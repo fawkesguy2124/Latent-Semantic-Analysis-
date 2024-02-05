8929dc1784e19b38da2179cdd109c38b9cb20c8f
fawkesguy2124 <139849573+fawkesguy2124@users.noreply.github.com>
2024-02-05 23:45:19 +0530

    Create LSA.py

---
+++ LSA.py
@@ -0,0 +1,45 @@
+import os
+import pandas as pd
+from nltk.tokenize import RegexpTokenizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import TruncatedSVD
+
+documents_list = []
+with open( os.path.join("articles+4.txt") ,"r") as fin:
+    for line in fin.readlines():
+        text = line.strip()
+        documents_list.append(text)
+
+tokenizer = RegexpTokenizer(r'\w+')
+
+
+tfidf = TfidfVectorizer(lowercase=True,
+                        stop_words='english',
+                        ngram_range = (1,1),
+                        tokenizer = tokenizer.tokenize)
+
+
+train_data = tfidf.fit_transform(documents_list)
+
+
+num_components=10
+
+
+lsa = TruncatedSVD(n_components=num_components, n_iter=100, random_state=42)
+
+
+lsa.fit_transform(train_data)
+
+
+Sigma = lsa.singular_values_
+V_transpose = lsa.components_.T
+
+
+
+terms = tfidf.get_feature_names_out()
+
+for index, component in enumerate(lsa.components_):
+    zipped = zip(terms, component)
+    top_terms_key=sorted(zipped, key = lambda t: t[1], reverse=True)[:5]
+    top_terms_list=list(dict(top_terms_key).keys())
+    print("Topic "+str(index)+": ",top_terms_list)
